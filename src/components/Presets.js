1f7c855e27fdb36710b0853b1da739381bd8f828
patrick-replogle <patrickr1138@gmail.com>
2020-08-19 14:33:45 -0700

    worked on the presets components a bit. need to do a refactor to perform deep clones of the 2d array to avoid mutating the original while updating the copy

--- src/components/Presets.js
+++ src/components/Presets.js
@@ -1,32 +1,40 @@
 import React from "react";
 
+import { createNewBoard } from "../functions";
+
 const Presets = ({ gridSize, setBoard }) => {
-  const createRandomBoard = (size) => {
+  const generateRandomBoard = (size) => {
     let mainArray = [];
 
     for (let i = 0; i < size; i++) {
       let subArray = [];
-
       for (let j = 0; j < size; j++) {
         subArray.push({ status: Math.round(Math.random()), location: [i, j] });
       }
       mainArray.push(subArray);
     }
-
     return mainArray;
+  };
+
+  const presetTwo = (size) => {
+    let newBoard = createNewBoard(size);
+    newBoard[5][12]["status"] = 1;
+    newBoard[5][13]["status"] = 1;
+    newBoard[5][14]["status"] = 1;
+    setBoard(newBoard);
   };
 
   return (
     <div style={{ marginTop: "20px" }}>
       <div style={{ display: "flex" }}>
-        <button onClick={() => setBoard(createRandomBoard(gridSize))}>
+        <button onClick={() => setBoard(generateRandomBoard(gridSize))}>
           Preset 1
         </button>
         <p>Preset 1 will generate a random board of live and dead cells</p>
       </div>
 
       <div style={{ display: "flex" }}>
-        <button>Preset 2</button>
+        <button onClick={() => presetTwo(gridSize)}>Preset 2</button>
         <p>description</p>
       </div>
 
