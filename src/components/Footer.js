d3eda904639433a6a70db762abd6a4b996a71fe8
patrick-replogle <patrickr1138@gmail.com>
2020-08-27 12:50:46 -0700

    updated footer to inlude contact links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import { Icon } from "@iconify/react";
+import githubFilled from "@iconify/icons-ant-design/github-filled";
+import portfolioIcon from "@iconify/icons-bytesize/portfolio";
+import linkedinFilled from "@iconify/icons-ant-design/linkedin-filled";
+
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="http://patrick-replogle.com" target="blank">
-        Patrick Replogle 2020
+      <a
+        href="https://github.com/patrick-replogle/Conways-Game-of-Life"
+        target="blank"
+        style={{ margin: "0 3%" }}
+      >
+        <Icon icon={githubFilled} style={{ fontSize: "45px" }} />
+      </a>
+
+      <a
+        href="https://patrick-replogle.com/"
+        target="blank"
+        style={{ margin: "0 3%" }}
+      >
+        <Icon icon={portfolioIcon} style={{ fontSize: "40px" }} />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/patrick-replogle/"
+        target="blank"
+        style={{ margin: "0 3%" }}
+      >
+        <Icon icon={linkedinFilled} style={{ fontSize: "45px" }} />
       </a>
     </div>
   );
